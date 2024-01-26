05524e2cf36b2c533f0c13a8357d204644477c64
Leo KUANG <leokuang4@gmail.com>
2024-01-26 19:27:12 +1100

    Checkin Q88

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Questions;
+﻿using Questions.ArrayAndString;
+
+namespace Questions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Q088
+        var question = new Q088();
+        question.Merge(new int[] { 1, 2, 3, 0, 0, 0 }, 2, new int[] { 2, 5, 6 }, 3);
+
     }
 }
 
