1b3aa17fd42552b0860aa7390a68fbc98bd2471c
Leo KUANG <leokuang4@gmail.com>
2024-01-29 11:11:45 +1100

    Checkin Q274

--- Program.cs
+++ Program.cs
@@ -6,9 +6,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Q088
-        var question = new Q088();
-        question.Merge(new int[] { 1, 2, 3, 0, 0, 0 }, 2, new int[] { 2, 5, 6 }, 3);
+        ////Q088
+        //var question = new Q088();
+        //question.Merge(new int[] { 1, 2, 3, 0, 0, 0 }, 2, new int[] { 2, 5, 6 }, 3);
+
+        ////Q045
+        //var question = new Q045();
+        //question.Jump(new int[] { 2, 3, 1, 1, 4 });
+
+        //Q274
+        var question = new Q274();
+        question.HIndex(new int[] { 3, 0, 6, 1, 5 });
 
     }
 }
