ab5992805849095b99f090c9dea34fbfddd7199d
Leo KUANG <leokuang4@gmail.com>
2024-02-17 20:31:53 +1100

    Check-in Q134

--- Program.cs
+++ Program.cs
@@ -18,9 +18,13 @@ class Program
         //var question = new Q274();
         //question.HIndex(new int[] { 3, 0, 6, 1, 5 });
 
-        //Q238
-        var question = new Q238();
-        question.ProductExceptSelf(new int[] { 1, 2, 3, 4 });
+        ////Q238
+        //var question = new Q238();
+        //question.ProductExceptSelf(new int[] { 1, 2, 3, 4 });
+
+        //Q134
+        var question = new Q134();
+        question.CanCompleteCircuit(new int[] { 1, 2, 3, 4, 5 }, new int[] { 3, 4, 5, 1, 2 });
 
     }
 }
