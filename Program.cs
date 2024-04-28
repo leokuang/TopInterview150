bd8cddf957afa7b21dfeb5dd6eb3c32c3c28f2d2
Leo KUANG <leokuang4@gmail.com>
2024-04-28 21:09:29 +1000

    Push Q704

--- Program.cs
+++ Program.cs
@@ -23,9 +23,14 @@ class Program
         //question.ProductExceptSelf(new int[] { 1, 2, 3, 4 });
 
         //Q134
-        var question = new Q134();
-        question.CanCompleteCircuit(new int[] { 1, 2, 3, 4, 5 }, new int[] { 3, 4, 5, 1, 2 });
+        // var question = new Q134();
+        // question.CanCompleteCircuit(new int[] { 1, 2, 3, 4, 5 }, new int[] { 3, 4, 5, 1, 2 });
 
+        //Q704
+        var question = new Q704();
+        var result = question.Search(new int[] { -1, 0, 3, 5, 9, 12 }, 9);
+
+        Console.WriteLine($"Result is: {result}");
     }
 }
 
